4e8b8c525968e54131c264b7cd7666c762aba7fa
mihret1 <mihlet2@gmail.com>
2023-06-13 20:42:29 +0300

    fetch restaurant list from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,19 @@ import {Routes,BrowserRouter, Route} from 'react-router-dom'
 import {Box,Stack } from '@mui/material'
 import NavBar from "../src/components/NavBar/NavBar"
 import Home from "../src/components/Home/Home"
-
+import Auth from './components/Auth/Auth'
 function App() {
   return (
-
+      <BrowserRouter>
            <Box >
                <NavBar />
-               <Home />
+               <Routes>
+                 <Route exact={true} path='/' element={<Home />}/>
+                 <Route path='/auth' element={<Auth />}/>
+               </Routes>
            </Box>
+
+      </BrowserRouter>
            
   );
 }
---
+++ frontend/src/components/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+
+
+export default function Auth(){
+    return(
+        <Box bgcolor='red'>
+            <Box> login form</Box>
+        </Box>
+    )
+
+}

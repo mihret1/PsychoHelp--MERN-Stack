8872a92bde82d1e42b09d912c297d25988b774ad
mihret1 <mihlet2@gmail.com>
2023-12-08 17:08:06 +0300

    add ref for scrolling comments

--- frontend/src/components/postDetail/commentSection.jsx
+++ frontend/src/components/postDetail/commentSection.jsx
@@ -1,12 +1,16 @@
-import { Button, Stack, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import { Box, Button, Stack, TextField } from '@mui/material'
+import React, { useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { postComment } from '../../actions/posts'
+
+
+
 
 const CommentSection=({post})=> {
     const [comment,setComment]=useState('')
     const [comments,setComments]=useState(post.comments)
     const user=JSON.parse(localStorage.getItem('profile'))
+    const commentRef=useRef()
 
     const dispatch=useDispatch()
 
@@ -16,26 +20,35 @@ const CommentSection=({post})=> {
         const newComment= await dispatch(postComment(finalComment,post._id))
         setComments(newComment)
         setComment('')
-    }
+        commentRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
 
   return (
     <div>
-          <Stack>
+      <Stack direction='row' spacing={2}>
+            <Stack>
             comments
+
             {comments?.map((c,i)=>(
             <h3>{c}</h3>
             ))}
-        </Stack>
-        <h1>write comments here bro</h1>
+            <div ref={commentRef}/>
+          </Stack>
+    <Box>
+      <h1>write comments here bro</h1>
     
-        <TextField 
-           onChange={(e)=>setComment(e.target.value)} 
-           value={comment}
-           label='write ur comment'/>
-           
-        <Button variant='contained' onClick={handleComment}>
-                 comment
-        </Button>
+    <TextField 
+       onChange={(e)=>setComment(e.target.value)} 
+       value={comment}
+       label='write ur comment'/>
+       
+    <Button variant='contained' onClick={handleComment}>
+             comment
+    </Button>
+  </Box>
+
+        </Stack>
+       
     </div>
   )
 }
